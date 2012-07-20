b8799b87d49e0035a1dfd3792d275082a5dae526
Ricardo Caldeira <ricardo.nezz@gmail.com>
2012-07-20 18:33:27 -0300

    Finish Section 10.1.4

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,8 @@
 #  created_at      :datetime        not null
 #  updated_at      :datetime        not null
 #  password_digest :string(255)
+#  remember_token  :string(255)
+#  admin           :boolean         default(FALSE)
 #
 
 require 'spec_helper'
@@ -16,7 +18,7 @@ describe User do
 
   before do 
     @user = User.new(name: "Example User", email: "user@example.com",
-                     password: "foobar", password_confirmation: "foobar")
+    password: "foobar", password_confirmation: "foobar")
   end
 
   subject { @user }
@@ -28,25 +30,27 @@ describe User do
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
   it { should respond_to(:remember_token) }
-  
+
   it { should respond_to(:admin) }
   it { should respond_to(:authenticate) }
 
+  it { should respond_to(:microposts) }
+
   it { should be_valid }
   it { should_not be_admin }
-  
+
   describe "with admin attribute set to 'true'" do
     before { @user.toggle!(:admin) }
-    
+
     it { should be_admin }
-    
+
   end
 
   describe "remember token" do
     before { @user.save }
     its(:remember_token) { should_not be_blank }
   end
-  
+
   describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
@@ -61,7 +65,7 @@ describe User do
     before { @user.email = " " }
     it { should_not be_valid }
   end
-  
+
   describe "when password is not present" do
     before { @user.password = @user.password_confirmation = " " }
     it { should_not be_valid }
@@ -91,40 +95,63 @@ describe User do
       end
     end
   end
-  
+
   describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
       user_with_same_email.email = @user.email.upcase
       user_with_same_email.save
     end
-    
+
     it { should_not be_valid }
   end
-  
+
   describe "when password doesn't match confirmation" do
     before { @user.password_confirmation = "mismatch" }
     it { should_not be_valid }
   end
-  
+
   describe "when password confirmation is nil" do
     before { @user.password_confirmation = nil }
     it { should_not be_valid }
   end
-  
+
   describe "return value of authenticate method" do
     before { @user.save }
     let(:found_user) { User.find_by_email(@user.email) }
-    
+
     describe "with valid password" do
       it { should == found_user.authenticate(@user.password) }
     end
-    
+
     describe "with invalid password" do
       let(:user_for_invalid_password) { found_user.authenticate("invalid") }
-      
+
       it { should_not == user_for_invalid_password }
       specify { user_for_invalid_password.should be_false }
+    end
+  end
+
+  describe "micropost associations" do
+
+    before { @user.save }
+    let!(:older_micropost) do 
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    it "should have the right microposts in the right order" do
+      @user.microposts.should == [newer_micropost, older_micropost]
+    end
+
+    it "should destroy associated microposts" do
+      microposts = @user.microposts
+      @user.destroy
+      microposts.each do |micropost|
+        Micropost.find_by_id(micropost.id).should be_nil
+      end
     end
   end
 
